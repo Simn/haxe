cd1d1dafd72d8b0e3b3a1f7394ac148caa6f0455
Simon Krajewski <simon@haxe.org>
2017-09-30 11:23:12 +0200

    deprecate `implements Dynamic`

--- src/typing/typeload.ml
+++ src/typing/typeload.ml
@@ -1489,6 +1489,7 @@ module Inheritance = struct
 					)
 				| TDynamic t ->
 					if c.cl_dynamic <> None then error "Cannot have several dynamics" p;
+					if not c.cl_extern then ctx.com.warning "`implements Dynamic` is deprecated for non-extern classes and will be removed soon" p;
 					c.cl_dynamic <- Some t;
 					(fun () -> ())
 				| _ ->
